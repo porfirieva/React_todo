b3ad4fbc54017c2b2e905959f361f6eada0b80f4
Ksenia Porfirieva <porfirieva.k@mail.ru>
2022-08-22 18:33:55 +0600

    change

--- Weblayout/Lionic/js/script.js
+++ Weblayout/Lionic/js/script.js
@@ -1,3 +1,4 @@
+// бургер-меню по клику
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.header__nav');
 let menuLinks = menu.querySelectorAll('.nav__link');
@@ -15,5 +16,38 @@ menuLinks.forEach(function(el){
     burger.classList.remove('burger--active');
     menu.classList.remove('header__nav--active');
     document.body.classList.remove('stop-scroll')
+  });
+});
+
+// показать больше в блоке articles
+const btnMore = document.querySelector('.articles-more');
+const articlesItems = document.querySelectorAll('.articles__item');
+
+btnMore.addEventListener('click', () => {
+  articlesItems.forEach(element => {
+    element.classList.add('articles__item-visible')
+  });
+
+  btnMore.closest('.articles-center').classList.add('articles-center--hidden')
+})
+
+// таб по ховеру на карточки services
+let tabServices = document.querySelectorAll('.services__item');
+let tabText = document.querySelectorAll('.services__hide-block')
+
+tabServices.forEach(el => {
+  el.addEventListener('mouseover', function(e) {
+    const path = e.currentTarget.dataset.path;
+
+    tabServices.forEach(function(item) {
+      item.classList.remove('services__item-active')
+    });
+    e.currentTarget.classList.add('services__item-active')
+
+    tabText.forEach(function(element) {
+      element.classList.remove('services__hide-block-active')
+    });
+
+    document.querySelector(`[data-target="${path}"]`).classList.add('services__hide-block-active')
   });
 });
