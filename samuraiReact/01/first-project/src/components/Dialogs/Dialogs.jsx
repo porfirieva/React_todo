ee2a5675246b752fda789375b76a34d9c9856c9b
Ksenia Porfirieva <porfirieva.k@mail.ru>
2022-09-06 22:09:22 +0600

    add styles

--- samuraiReact/01/first-project/src/components/Dialogs/Dialogs.jsx
+++ samuraiReact/01/first-project/src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,20 @@
-// import style from "./Dialogs.module.css";
+import style from "./Dialogs.module.css";
 
 const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={style.dialogs}>
+      <ul className={style.list}>
+        <li className={style.name}>Ksu</li>
+        <li className={style.name}>Pasha</li>
+        <li className={style.name}>Vanya</li>
+      </ul>
+      <ul className={style.messeges}>
+        <li className={style.messege}>Hello!</li>
+        <li className={style.messege}>How are u?</li>
+        <li className={style.messege}>Hi!</li>
+      </ul>
+    </div>
+  )
 };
 
 export default Dialogs;
